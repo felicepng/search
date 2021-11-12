9bad60942d041a85dc0fa1443e34836912755c40
Felice <felicepng2010@gmail.com>
2021-11-12 23:45:40 +0800

    Add suggestions menu

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -12,28 +12,44 @@ const SearchBar = () => {
         <img src="/logo.png" className="w-6 mr-2" />
         An Official Website of the <span className="font-semibold ml-1"> Singapore Government</span>
       </div>
-      <div className="text-xl bg-white shadow-search h-40 px-44 py-12">
-        <div className={`flex justify-between h-full border rounded-lg ${focused ? 'border-theme' : 'border-gray-400'}`}>
-          <div className="flex w-full jusify-between items-center px-7"
+
+      <div className="absolute z-50 text-lg flex flex-col w-full items-center px-44 pt-12">
+        <div className={`flex items-center justify-center h-16 w-full border rounded-lg ${focused ? 'border-theme' : 'border-gray-400'}`}>
+          <div className="flex flex-col w-full justify-between items-center px-7"
             onClick={() => setFocused(true)}
             onBlur={() => setFocused(false)}
           >
-            <input placeholder="Search..." value={searchText} className="focus:outline-none rounded-lg w-full"
-              onChange={e => setSearchText(e.target.value)}
-            />
-            {
-              searchText !== "" &&
-              <MdOutlineClear className="w-6 h-6 text-secondary hover:text-black cursor-pointer"
-                onClick={() => setSearchText("")}
+            <div className="flex w-full">
+              <input placeholder="Search..." value={searchText} className="focus:outline-none rounded-lg w-full"
+                onChange={e => setSearchText(e.target.value)}
               />
-            }
+              {
+                searchText !== "" &&
+                <MdOutlineClear className="w-6 h-6 text-secondary hover:text-black cursor-pointer"
+                  onClick={() => setSearchText("")}
+                />
+              }
+            </div>
           </div>
-          <div className="font-medium cursor-pointer px-10 h-full flex items-center justify-center bg-theme hover:bg-blue-700 rounded-md text-white">
+          <div className="font-medium cursor-pointer w-44 h-full flex items-center justify-center bg-theme hover:bg-blue-700 rounded-md text-white">
             <IoSearchSharp className="w-6 h-6 text-white mr-2" />
             Search
           </div>
         </div>
+
+        {
+          searchText.length > 2 &&
+          <div className="flex items-center justify-center w-full">
+            <div className="bg-white shadow-md rounded-b-lg h-96 w-full">
+              Suggestions
+            </div>
+            <div className="w-44" />
+          </div>
+        }
+
       </div>
+
+      <div className="bg-white shadow-search flex w-full items-center h-40 px-44" />
     </div>
   )
 }
