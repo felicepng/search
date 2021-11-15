fc4ac191bb4b8defd5910c2f32f50e3e87ca8cb8
Felice <felicepng2010@gmail.com>
2021-11-15 10:17:07 +0800

    Add methods to account for search term substrings

--- utils/search/getSearchIndices.js
+++ utils/search/getSearchIndices.js
@@ -1,15 +1,21 @@
 import _ from "lodash";
 
 export const getSearchIndices = (excerpt, searchQuery) => {
-  // split into array in the case that search query has multiple words
-  var searchTerms = searchQuery.split(" ");
+  var searchTerms = searchQuery.split(" ");  // split into array in the case that search query has multiple words
+  _.remove(searchTerms, searchTerm => { return searchTerm.length === 0 });  // remove empty string, in the case of multiple whitespaces
+  searchTerms = _.reverse(_.sortBy(searchTerms, [i => { return i.length; }]));  // sort by descending length of search term
 
-  // remove empty string, in the case of multiple whitespaces
-  _.remove(searchTerms, searchTerm => { return searchTerm.length < 3 });
-  
-  const indices = [];
+  const noSubstrings = [];  // create array to remove substrings of search terms
 
   searchTerms.map(searchTerm => {
+    if (!noSubstrings.some(i => i.includes(searchTerm))) {
+      noSubstrings.push(searchTerm);
+    }
+  });
+
+  const indices = [];  // array to store start and end indices of each term
+
+  noSubstrings.map(searchTerm => {
     // add start and end points to array for each search term
     indices.push([...excerpt.matchAll(new RegExp(searchTerm, 'gi'))].map(i => [i.index, i.index + searchTerm.length]));
   })
