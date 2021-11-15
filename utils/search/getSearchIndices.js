d1abd61e16c7ade6180153136c3c990b704643c7
Felice <felicepng2010@gmail.com>
2021-11-15 10:48:35 +0800

    Add method to remove overlapping search term indices

--- utils/search/getSearchIndices.js
+++ utils/search/getSearchIndices.js
@@ -3,12 +3,12 @@ import _ from "lodash";
 export const getSearchIndices = (excerpt, searchQuery) => {
   var searchTerms = searchQuery.split(" ");  // split into array in the case that search query has multiple words
   _.remove(searchTerms, searchTerm => { return searchTerm.length === 0 });  // remove empty string, in the case of multiple whitespaces
-  searchTerms = _.reverse(_.sortBy(searchTerms, [i => { return i.length; }]));  // sort by descending length of search term
+  searchTerms = _.reverse(_.sortBy(searchTerms, [o => { return o.length; }]));  // sort by descending length of search term
 
   const noSubstrings = [];  // create array to remove substrings of search terms
 
   searchTerms.map(searchTerm => {
-    if (!noSubstrings.some(i => i.includes(searchTerm))) {
+    if (!noSubstrings.some(o => o.includes(searchTerm))) {
       noSubstrings.push(searchTerm);
     }
   });
@@ -17,12 +17,19 @@ export const getSearchIndices = (excerpt, searchQuery) => {
 
   noSubstrings.map(searchTerm => {
     // add start and end points to array for each search term
-    indices.push([...excerpt.matchAll(new RegExp(searchTerm, 'gi'))].map(i => [i.index, i.index + searchTerm.length]));
+    indices.push([...excerpt.matchAll(new RegExp(searchTerm, 'gi'))].map(o => [o.index, o.index + searchTerm.length]));
   })
 
-  const flattened = _.flatten(indices);  // fllatten array by one level
+  const sorted = _.sortBy(_.flatten(indices), [o => { return o[0]; }]);  // flatten array by one level and sort by ascending index order
 
-  return _.sortBy(flattened, [i => { return i[0]; }]);  // sort array before returning
+  // remove overlapping search term indices
+  _.remove(sorted, (o, index) => {
+    if (index !== 0 && o[0] < sorted[index - 1][1]) {
+      return true;
+    }
+  })
+
+  return sorted;
 }
 
 module.exports = getSearchIndices;
